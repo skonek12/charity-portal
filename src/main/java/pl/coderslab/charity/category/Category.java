89a6d7fb32664df76cedcd72ae4e0c7d59c65e6d
skonek12 <lukasz.skoniecki1@gmail.com>
2023-03-08 17:48:40 +0100

    4

---
+++ src/main/java/pl/coderslab/charity/category/Category.java
@@ -0,0 +1,4 @@
+package pl.coderslab.charity.category;
+
+public class Category {
+}
---
+++ src/main/java/pl/coderslab/charity/donation/Donation.java
@@ -0,0 +1,12 @@
+package pl.coderslab.charity.category;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
